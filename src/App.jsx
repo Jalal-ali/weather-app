58c8f68ecb94b417e4b97ea811f3c92ba57517e1
Jalal Ali <alikhanumar795@gmail.com>
2024-09-06 03:01:48 +0500

    done

--- src/App.jsx
+++ src/App.jsx
@@ -13,17 +13,23 @@ const App = ()=>{
   
   const getData = async ()=>{
     const res = await axios(`http://api.weatherapi.com/v1/current.json?key=6603ca9e8f34494282d81834240409&q=${inputRef.current.value}&aqi=no`)
-    setData([res.data])
+    setData([res.data, ...data])
     inputRef.current.value = null ;
   }
+  console.log(data);
+  
 
   
 
   return (
     <>
+    <h1 class="mb-5 mt-2 bg-clip-text text-transparent text-center bg-gradient-to-r from-teal-700 via-lime-400 to-cyan-500  text-5xl font-black">
+    Weather App
+</h1>
+    <div className="flex flex-wrap justify-content">
+
 <div className="relative w-full max-w-xl mt-4 mx-auto bg-white rounded-full">
   <form onSubmit={searchWeather}>
-
   <input ref={inputRef} placeholder="Search weather e.g. karachi , islamabad" className="rounded-full w-full h-16 bg-transparent py-2 pl-8 pr-32 outline-none border-2 border-gray-100 shadow-md hover:outline-none focus:ring-teal-200 focus:border-teal-200" type="text" name="query" id="query"/>
   <button type="submit" className="absolute inline-flex items-center h-10 px-4 py-2 text-sm text-white transition duration-150 ease-in-out rounded-full outline-none right-3 top-3 bg-teal-600 sm:px-6 sm:text-base sm:font-medium hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500">
     <svg className="-ml-0.5 sm:-ml-1 mr-2 w-4 h-4 sm:h-5 sm:w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -34,9 +40,9 @@ const App = ()=>{
   </form>
 </div>
 
-    {data.length > 0 ? data.map((item) => {
-      return <div key={item.location.name} className="w-full h-screen flex items-center justify-center">
-        <div className="bg-slate-300  p-8 bg-opacity-80 rounded-3xl flex space-x-12 items-center shadow-lg border-t-2 border-green-300">
+<div  className="w-full flex flex-wrap items-center justify- justify-center">
+    {data.length > 0 ? data.map((item , index) => {
+      return <div key={index} className="bg-slate-300 m-3 p-8 bg-opacity-80 rounded-3xl flex space-x-12 items-center shadow-lg border-t-2 border-green-300">
           <div>
            <img src={item.current.condition.icon} alt="img" />
             <p className="text-center text-gray-500 mt-2 text-sm">{item.current.condition.text}</p>
@@ -46,9 +52,14 @@ const App = ()=>{
             <p className="text-gray-500 text-sm">{item.location.name + ',' + item.location.country}</p>
           </div>
         </div> 
-      </div>}) : <h2>search</h2>}
+      }) : <h1
+      class="text-4xl mt-28 text-center font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-sky-800 via-lime-600 to-cyan-700">
+      No Weather Found.
+  </h1>}
+      </div>
    
    
+    </div>
     </>
   )
 }
