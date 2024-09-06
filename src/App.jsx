befa0616a903d986bd769863ba6c19f417fbac43
Jallluu <alikhanumar795@gmail.com>
2024-09-06 18:07:40 +0500

    check

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,60 @@ import axios from "axios"
 import { useRef, useState } from "react";
 
 const App = ()=>{
-  const [data, setData] = useState([]);
-  const inputRef = useRef();
+  // const [data, setData] = useState([]);
+  // const inputRef = useRef();
 
-  const searchWeather = async (event)=>{
+  // const searchWeather = async (event)=>{
+  //   event.preventDefault();
+  //   console.log(inputRef.current.value);
+  //   const res = await axios(`http://api.weatherapi.com/v1/current.json?key=6603ca9e8f34494282d81834240409&q=${inputRef.current.value}&aqi=no`)
+  //   setData([res.data, ...data])
+  //   inputRef.current.value = null 
+  // } 
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  //use ref for getting form value
+  const inputRef = useRef(null);
+
+  //show weather function
+  const showWeather = (event) => {
     event.preventDefault();
-    console.log(inputRef.current.value);
-    const res = await axios(`http://api.weatherapi.com/v1/current.json?key=6603ca9e8f34494282d81834240409&q=${inputRef.current.value}&aqi=no`)
-    setData([res.data, ...data])
-  } 
-  inputRef.current.value = null ;
-  
+    setLoading(true)
+    const key = 'b4db485d7c4c485fa6d84351232508'
+    axios.get(`https://api.weatherapi.com/v1/current.json?key=${key}&q=${inputRef.current.value}&aqi=no`)
+      .then((res) => {
+        // console.log('res==>', res.data);
+        setData([res.data, ...data]);
+        inputRef.current.value = '';
+        setLoading(false)
+      })
+      .catch(() => {
+        // console.log(err);
+        alert('no such city');
+        inputRef.current.value = '';
+        setLoading(false)
+      })
+  }
   
 
   
 
   return (
     <>
-    <h1 className="mb-5 mt-2 bg-clip-text text-transparent text-center bg-gradient-to-r from-teal-700 via-lime-400 to-cyan-500  text-5xl font-black">
+    <div className='h-[100vh]'>
+      <h1 className="text-3xl text-center mt-2  font-extrabold">
+        Weather App
+      </h1>
+      <form onSubmit={showWeather}>
+      <div className='w-[50%] mx-auto mt-[3%]'>
+        <input ref={inputRef} className="focus:ring-2 focus:ring-blue-500 focus:outline-none appearance-none w-full text-sm leading-6 text-slate-900 placeholder-slate-400 rounded-md py-2 pl-10 ring-1 ring-slate-200 shadow-sm" type="text" aria-label="Filter projects" placeholder="City Name" />
+      </div>
+      <div className='text-center'>
+        <button  type='submit' className="bg-blue-500 text-white text-sm leading-6 font-medium py-2 px-3 rounded-lg mt-5">{loading ? 'loading...' : 'Show Weather'}</button>
+      </div>
+      </form>
+    {/* <h1 className="mb-5 mt-2 bg-clip-text text-transparent text-center bg-gradient-to-r from-teal-700 via-lime-400 to-cyan-500  text-5xl font-black">
     Weather App
 </h1>
     <div className="flex flex-wrap justify-content">
@@ -34,7 +70,7 @@ const App = ()=>{
     Search
   </button>
   </form>
-</div>
+</div> */}
 
 <div  className="w-full flex flex-wrap items-center justify- justify-center">
     {data.length > 0 ? data.map((item , index) => {
