c0ef0fb3d61d0d2d8205ec84214145859550aa31
Jallluu <alikhanumar795@gmail.com>
2025-02-10 04:59:32 +0500

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -30,41 +30,47 @@ const App = ()=>{
 
   return (
     <>
-<div className="bg-cover py-3 px-3 min-h-screen bg-no-repeat bg-[url('https://static.vecteezy.com/system/resources/previews/036/226/616/large_2x/ai-generated-nature-landscapes-background-free-photo.jpg')]">
-    <h1 className="mb-5 bg-clip-text text-transparent text-center bg-gradient-to-b from-emerald-500 to-lime-400  text-5xl font-black">
-    Weather App
-</h1>
+<div className="bg-cover bg-center min-h-screen bg-no-repeat py-3 px-3" style={{ backgroundImage: "url('https://static.vecteezy.com/system/resources/previews/036/226/616/large_2x/ai-generated-nature-landscapes-background-free-photo.jpg')" }}>
+    <h1 className="mb-5 bg-clip-text text-transparent text-center bg-gradient-to-b from-emerald-500 to-lime-400 text-5xl font-black">
+        Weather App
+    </h1>
 
-<div className="relative w-full max-w-xl mt-4 md:mx-auto lg:mx-auto xl:mx-auto bg-white rounded-full">
-  <form onSubmit={searchWeather}>
-  <input ref={inputRef} placeholder="Search weather e.g. Karachi , Islamabad" className="rounded-full w-full h-16 bg-transparent  py-2 pl-8 pr-32 outline-none border-2 border-gray-100 shadow-md hover:outline-none focus:ring-teal-200 focus:border-teal-200" type="text" name="query" id="query" required/>
-  <button type="submit" className="absolute inline-flex items-center h-10 px-4 py-2 text-sm text-white transition duration-150 ease-in-out rounded-full outline-none right-3 top-3 bg-teal-600 sm:px-6 sm:text-base sm:font-medium hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500">
-    <svg className="-ml-0.5 sm:-ml-1 mr-2 w-4 h-4 sm:h-5 sm:w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-      <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-    </svg>{loading ? "Loading" : "Search"}
-  </button>
-  </form>
+    <div className="relative w-full max-w-xl mt-4 md:mx-auto bg-white rounded-full">
+        <form onSubmit={searchWeather}>
+            <input ref={inputRef} placeholder="Search weather e.g. Karachi, Islamabad" 
+                className="rounded-full w-full h-16 bg-transparent py-2 pl-8 pr-32 outline-none border-2 border-gray-100 shadow-md focus:ring-teal-200 focus:border-teal-200" 
+                type="text" name="query" id="query" required />
+            <button type="submit" className="absolute inline-flex items-center h-10 px-4 py-2 text-sm text-white transition duration-150 ease-in-out rounded-full outline-none right-3 top-3 bg-teal-600 sm:px-6 sm:text-base sm:font-medium hover:bg-teal-700 focus:ring-2 focus:ring-offset-2 focus:ring-teal-500">
+                <svg className="-ml-0.5 sm:-ml-1 mr-2 w-4 h-4 sm:h-5 sm:w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+                </svg>
+                {loading ? "Loading" : "Search"}
+            </button>
+        </form>
+    </div>
+
+    <div className={`w-full my-6 ${data.length > 0 ? "border-t-2 rounded-t border-opacity-30 border-cyan-700 overflow-y-auto" : ""} flex flex-wrap items-center justify-center max-h-[500px] scrollbar-hidden`}>
+
+        {data.length > 0 ? data.map((item, index) => (
+            <div key={index} className="flex flex-wrap justify-center m-5">
+                <Card src={item.current.condition.icon} text={item.current.condition.text} temp={item.current.temp_c} name={item.location.name} country={item.location.country} />
+            </div>
+        )) : loading ? (
+            <div role="status" className="mt-28">
+                <svg aria-hidden="true" className="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                    <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+                </svg>
+                <span className="sr-only">Loading...</span>
+            </div>
+        ) : (
+            <h1 className="text-4xl mt-28 text-center font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-green-800 via-gray-950 to-lime-700">
+                No Weather Found.
+            </h1>
+        )}
+    </div>
 </div>
 
-<div className="w-full flex flex-wrap items-center z-10 justify-center">
-    {data.length > 0 ? data.map((item , index) => {
-      return <div key={index} className="flex flex-wrap justify-center m-5">
-          <Card src={item.current.condition.icon} text={item.current.condition.text} temp={item.current.temp_c} name={item.location.name} country={item.location.country} />
-        </div> 
-      }) : loading ? <div role="status">
-      <svg aria-hidden="true" className="inline mt-28 w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
-        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
-    </svg>
-      <span className="sr-only">Loading...</span>
-  </div> : <h1
-      className="text-4xl mt-28 text-center font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-green-800 via-gray-950 to-lime-700">
-      No Weather Found.
-  </h1>}
-      </div>
-   
-   
-    </div>
     </>
   )
 }
